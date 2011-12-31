3a363f7b00530568925da8134ec6aaa0d0a3abd6
Luís Oliveira <loliveira@common-lisp.net>
2011-12-31 01:44:13 +0000

    Export current API.

--- package.lisp
+++ package.lisp
@@ -9,11 +9,43 @@
                 #:once-only
                 #:switch)
   (:export
+   ;; Agent
    #:*agent*
+   #:agent
    #:user-agent-aliases
    #:get
    #:reload
-   #:back))
+   #:back
+   #:last-response
+   #:saving-excursion
+
+   ;; Responses
+   #:response
+   #:get-header
+   #:response-header
+   #:status-code-of
+   #:method-of
+   #:uri-of
+   #:status-of
+   #:headers-of
+   #:content-of
+
+   #:file
+   #:page
+   #:html-page
+   #:xml-page
+   #:xhtml-page
+
+   ;; Parsing and Querying
+   #:form
+   #:forms
+   #:link
+   #:links
+   #:html-element
+   #:query
+   #:xquery
+   #:inner-text
+   ))
 
 (defpackage :mechanize-user
   (:use :cl :mechanize)
